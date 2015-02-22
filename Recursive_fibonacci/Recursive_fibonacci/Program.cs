e46168ec2b61efd05445a495a76ebf8953be2848
nur-3 <sarikayakubra@windowslive.com>
2015-02-22 16:56:02 +0200

    fibonacci_recursive

---
+++ Recursive_fibonacci/Recursive_fibonacci/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch sw = new Stopwatch(); // kodun çalışma süresini öğrenebilmek için stopwatch fonksiyonu kullanıldı.
+            Console.WriteLine("Kaçıncı fibonacci sayısı:");
+            int x = Convert.ToInt32(Console.ReadLine()); // bulunmak istenen sayı öğrenilip x'e atandı.
+
+            sw.Start(); // süre başlatıldı.
+            Console.WriteLine("{0}. fibonacci sayısı:", x);
+            Console.Write(Fib(x)); // fibonacci sayısı bulundu
+            sw.Stop(); // süre durduruldu
+
+            TimeSpan toplamSure = sw.Elapsed; //  süre değerine erişimi sağlayabilmek için timespan yapısı kullanıldı.
+            string sure = String.Format("{0}:{1}:{2}.{3}", toplamSure.Hours, toplamSure.Minutes, toplamSure.Seconds, toplamSure.TotalMilliseconds * 1000000);// süre bulunuldu.
+
+            Console.WriteLine();
+            Console.WriteLine("Toplam Süre:" + sure); // ekrana yazdırıldı.
+            Console.ReadLine(); // sure yi ekranda görebilmek için yazıldı.
+        }
+
+        public static int Fib(int a)
+        {
+            if (a == 0 | a == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return (Fib(a - 1) + Fib(a - 2));
+            }
+
+        }
+    }
+}
+ 
